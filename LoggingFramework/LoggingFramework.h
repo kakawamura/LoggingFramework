543e93ffd25b5969ead9a20699299a21b7c2fa73
waka-ka <wakaka2277@gmail.com>
2016-03-20 16:25:12 +0900

    Initial Commit

---
+++ LoggingFramework/LoggingFramework.h
@@ -0,0 +1,19 @@
+//
+//  LoggingFramework.h
+//  LoggingFramework
+//
+//  Created by 河村和始 on 3/20/16.
+//  Copyright © 2016 Kazushi Kawamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoggingFramework.
+FOUNDATION_EXPORT double LoggingFrameworkVersionNumber;
+
+//! Project version string for LoggingFramework.
+FOUNDATION_EXPORT const unsigned char LoggingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggingFramework/PublicHeader.h>
+
+
