1e742cb68948319fd844ec1bb2eec4df3da37d48
waka-ka <wakaka2277@gmail.com>
2016-03-20 16:42:52 +0900

    Initial commit

---
+++ LoggingFramework/Logging.swift
@@ -0,0 +1,28 @@
+//
+//  Logging.swift
+//  LoggingFramework
+//
+//  Created by 河村和始 on 3/20/16.
+//  Copyright © 2016 Kazushi Kawamura. All rights reserved.
+//
+
+import Foundation
+
+//public func Log(string: String) {
+//    print("[\(NSDate())] \(string)"
+//}
+//
+public class Logging {
+    public init() {
+        print("init")
+    }
+    
+    public func testMethod() {
+        print("test method")
+    }
+    
+    public class func testClassMethod() {
+        print("test clas method")
+    }
+    
+}
